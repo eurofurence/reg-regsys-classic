15ddee2a42e416a0a63addeb6062d788d0709231
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2023-12-07 16:45:12 +0100

    feat(#109): default config location for helm chart

--- src/java/org/eurofurence/regsys/backend/HardcodedConfig.java
+++ src/java/org/eurofurence/regsys/backend/HardcodedConfig.java
@@ -3,7 +3,7 @@ package org.eurofurence.regsys.backend;
 import org.eurofurence.regsys.backend.enums.RoomManagementOption;
 
 public class HardcodedConfig {
-    public static final String CONFIG_URL = "file:/develop/projects/eurofurence/reg-regsys-classic/config.yaml";
+    public static final String CONFIG_URL = "file:/config/config.yaml"; // location as provided by reg-helm-chart
 
     public static final long PAYMENT_CANCEL_HOURS = 48;
 
