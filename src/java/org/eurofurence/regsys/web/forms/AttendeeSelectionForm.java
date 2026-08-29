08d9b5b6cb4c8d5ca0907f7e8bda8e110b6144da
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2026-08-29 15:21:47 +0200

    feat(#369): prohibit bulk mail to cancelled and opted out

--- src/java/org/eurofurence/regsys/web/forms/AttendeeSelectionForm.java
+++ src/java/org/eurofurence/regsys/web/forms/AttendeeSelectionForm.java
@@ -48,7 +48,7 @@ public abstract class AttendeeSelectionForm extends Form {
     public void find() {
         try {
             AttendeeSearchCriteria criteria = CriteriaUtils.constructCriteriaByIdSet(idSet);
-            criteria.fillFields = Arrays.asList("balances", "nickname", "email", "status", "registration_language");
+            criteria.fillFields = Arrays.asList("balances", "nickname", "email", "status", "registration_language", "flags");
             attendeeResult = getPage().getAttendeeService().performFindAttendees(criteria, getPage().getTokenFromRequest(), getPage().getRequestId());
         } catch (DownstreamWebErrorException e) {
             resetErrors(Strings.searchForm.nobodyFound);
